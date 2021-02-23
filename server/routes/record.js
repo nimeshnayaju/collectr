031758351573b2053c1ed2ff2b1fbed4d856ded1
Nimesh Nayaju <nimeshnayaju@gmail.com>
2021-02-23 00:33:41 -0400

    Added Collection model and created unit tests for Collection (#2)
    * Added Collection model and added unit tests for Collection

--- server/routes/record.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const recordController = require('../controllers/recordController');
-
-router.get('/', recordController.getRecords);
-router.post('/add', recordController.addRecord);
-
-module.exports = router;
--- server/server.js
+++ server/server.js
@@ -7,13 +7,13 @@ require('./db/mongoose');
 
 // Routes
 const indexRoute = require('./routes/index');
-const recordRoutes = require('./routes/record');
+const collectionRoutes = require('./routes/collection');
 
 app.use(express.json()); // Parse application/json (recognize the incoming request object as a JSON object)
 
 // App route configuration
 app.use('/', indexRoute); // Index Route
-app.use('/records', recordRoutes); // Record Route
+app.use('/collections', collectionRoutes); // Collection Routes
 
 app.listen(config.PORT, () => console.log(`Listening on port: ${config.PORT}`));
 
