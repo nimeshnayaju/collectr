cb068d31866687612b8530056210ed4f44135043
Alex Little <aclittle@mta.ca>
2021-02-25 20:33:24 -0400

    added get specific item method

--- server/controllers/item.js
+++ server/controllers/item.js
@@ -1,2 +1,14 @@
 const Record = require('../models/item');
+const StatusCode = require('../helpers/constants');
 
+const getItem = async (req, res) -> {
+	const { id } = req.params;
+	
+	try {
+	  const item = await Item.findById(id); // <-- does this need '.populate()'?
+											// if so, what should be its param?
+	  res.status(StatusCode.OK).json(item);
+	} catch (err) {
+	  res.status(StatusCode.BAD_REQUEST).json({ message: err.message });
+	}	
+};
