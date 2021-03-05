4f8399f18402659b863d34d4fb5af8c80e57dbcf
nimeshnayaju <nimeshnayaju@gmail.com>
2021-03-05 16:23:23 -0400

    Added User model and blueprints for User controllers

---
+++ server/controllers/user.js
@@ -0,0 +1,27 @@
+const User = require('../models/user');
+const StatusCode = require('../helpers/constants');
+
+/**
+ * Logs a user in using their email and password
+ * @param req request object containing information about HTTP request
+ * @param res the response object used for sending back the desired HTTP response
+ * @returns {Promise<void>} the promise indicating success
+ */
+const login = async (req, res) => {
+    // Fill in here
+}
+
+/**
+ * Signs up a new user using their first name, last name, email, and password
+ * @param req request object containing information about HTTP request
+ * @param res the response object used for sending back the desired HTTP response
+ * @returns {Promise<void>} the promise indicating success
+ */
+ const signup = async (req, res) => {
+    // Fill in here
+}
+
+module.exports = {
+    login,
+    signup
+}
---
+++ server/routes/user.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+const userController = require('../controllers/user');
+
+router.post('/login', userController.login);
+router.post('/signup', userController.register);
+
+module.exports = router;
