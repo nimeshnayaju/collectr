a3133e76f7e916e0cfb50887da0365dd32dbb6b0
nimeshnayaju <nimeshnayaju@gmail.com>
2021-03-07 18:47:53 -0400

    Minor typo on Item router

--- server/routes/item.js
+++ server/routes/item.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router();
 const itemController = require('../controllers/item');
 
+router.get('/', itemController.getItems);
 router.post('/', itemController.addItem);
-router.get('/:id', itemController.getItems);
 router.put('/:id', itemController.updateItem);
 
 module.exports = router;
