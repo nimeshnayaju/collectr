dd9465eaacf0df4580791967235b2babac704784
Weiting Li <weitiii0819@gmail.com>
2021-03-09 14:56:00 -0400

    minor fix to the test

--- tests/user.test.js
+++ tests/user.test.js
@@ -9,53 +9,53 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 const newUser = {
-    firstname:'Ludwig',
-    lastname: 'Beethoven',
-    email: 'lbee@music.com',
-    password:'ilovepiano',
+  firstName: 'Ludwig',
+  lastName: 'Beethoven',
+  email: 'lbee@music.com',
+  password: 'ilovepiano',
 };
 
 describe('User Test', () => {
-    /**
+  /**
     * Test POST /signup
     */
-    before(async () => {
-        // Clear the user before the test
-        await User.deleteMany({});
-        // Add a mock Catalog object
-        const response = await chai
-                .request(app)
-                .post('/signup')
-                .send(newUser);
+  before(async () => {
+    // Clear the user before the test
+    await User.deleteMany({});
+    // Add a mock user object
+    const response = await chai
+      .request(app)
+      .post('/signup')
+      .send(newUser);
 
-        newUser.email = response.body.email;
+    newUser.email = response.body.email;
+  });
+
+  describe('POST /signup', () => {
+    it('should register a new user', async () => {
+      // Add the mock user object
+      const response = await chai
+        .request(app)
+        .post('/signup')
+        .send(newUser);
+
+      newUser.email = response.body.email;
+
+      response.should.have.status(statusCode.CREATED);
+      response.body.should.be.a('object');
+      response.body.should.have.property('firstName');
+      response.body.should.have.property('lastName');
+      response.body.should.have.property('email');
+      response.body.should.have.property('password');
     });
+  });
 
-    describe('POST /signup', () => {
-        it('should register a new user', async () => {
-            // Add the mock Item object
-            const response = await chai
-                    .request(app)
-                    .post('/signup')
-                    .send(newUser);
-
-            newUser.email = response.body.email;
-
-            response.should.have.status(statusCode.CREATED);
-            response.body.should.be.a('object');
-            response.body.should.have.property('firstName');
-            response.body.should.have.property('lastName');
-            response.body.should.have.property('email');
-            response.body.should.have.property('password');
-        });
-    });
-
-//     /**
-//     * Test POST /login
-//     */
-//     describe('POST /login', () => {
-//         it('should log a user in', async () => {
+  //     /**
+  //     * Test POST /login
+  //     */
+  //     describe('POST /login', () => {
+  //         it('should log a user in', async () => {
 
 //         });
 //     });
-})
+});
