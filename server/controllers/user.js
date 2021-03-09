4cc795fea79a91ec8d8f93a342976a410576da76
zsaralin <zsaralin@gmail.com>
2021-03-09 10:07:11 -0400

    added login

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,10 @@
 const User = require('../models/user');
 const StatusCode = require('../helpers/constants');
+const express = require("express");
+const { check, validationResult} = require("express-validator/check");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const router = express.Router();
 
 /**
  * Logs a user in using their email and password
@@ -8,7 +13,35 @@ const StatusCode = require('../helpers/constants');
  * @returns {Promise<void>} the promise indicating success
  */
 const login = async (req, res) => {
-    // Fill in here
+    check("email", "Enter email").isEmail();
+    check("password", "Enter password").isLength({min: 6})
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        });
+    }
+    const {email, password} = req.body;
+    try {
+        let user = await User.findOne({
+            email
+        });
+        if (!user)
+            return res.status(400).json({
+                message: "User Not Exist"
+            });
+        const isMatch = await bcrypt.compare(password, User.password);
+        if (!isMatch)
+            return res.status(400).json({
+                message: "Incorrect Password !"
+            });
+    }
+    catch (e) {
+        console.error(e);
+        res.status(500).json({
+            message: "Server Error"
+        });
+    }
 }
 
 /**
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
 
-router.post('/login', userController.login);
-router.post('/signup', userController.register);
+router.post('/login',userController.login)
+
+router.post('/signup', userController.signup);
 
 module.exports = router;
